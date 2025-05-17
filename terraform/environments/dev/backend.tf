e63c37f32681d5089102c573ac4c1f007883b8bf
Sai Teja D <saitejad@Sais-MacBook-Pro.local>
2025-05-18 02:52:06 +0530

    updated terraform dev backend with key-value pairs

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -1,9 +1,5 @@
-terraform {
-  backend "s3" {
-    bucket         = "hcl-bayer-terraform-state"
-    key            = "dev/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "hcl-bayer-terraform-lock"
-    encrypt        = true
-  }
-}
+bucket         = "hcl-bayer-terraform-state"
+key            = "dev/terraform.tfstate"
+region         = "us-east-1"
+dynamodb_table = "hcl-bayer-terraform-lock"
+encrypt        = true
